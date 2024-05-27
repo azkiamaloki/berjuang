00af6d7186a07d1f33a3824e836a1e1ebad46b62
azki <164864901+azkiamaloki@users.noreply.github.com>
2024-05-28 05:36:02 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-c90712f6-175d-4e97-912a-0add86afebfc/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-29c65789-8ba1-4fd7-acd9-fb37a1bf0403/)
 
 
 echo "$curl_output" >> Update.tsx
